a90488940e54eccb243681bdaf907a270ac9062f
Drew Gochenaur <drew@raincity.io>
2020-07-02 05:01:11 -0700

    new schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,13 +2,17 @@ CREATE DATABASE IF NOT EXISTS bindrev;
 
 USE bindrev;
 
-CREATE TABLE IF NOT EXISTS rev (
-    ip VARCHAR(64) NOT NULL PRIMARY KEY,
-    resolve VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS IPS (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ip VARCHAR(64) NOT NULL,
+    domain VARCHAR(256) NOT NULL,
+    latest BOOLEAN NOT NULL
 );
 
 CREATE USER IF NOT EXISTS 'bindrev'@'%' IDENTIFIED WITH mysql_native_password BY 'verdnib';
 GRANT SELECT, INSERT ON bindrev.rev TO 'bindrev'@'%';
+GRANT SELECT, INSERT, UPDATE ON bindrev.ips TO 'bindrev'@'%';
 
 CREATE USER IF NOT EXISTS 'bindrevro'@'%' IDENTIFIED WITH mysql_native_password BY 'verdnib';
 GRANT SELECT ON bindrev.rev TO 'bindrevro'@'%';
+GRANT SELECT ON bindrev.ips TO 'bindrevro'@'%';
